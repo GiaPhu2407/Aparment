223bce3bb5048551ce24b13950262a2ff088a8bd
GiaPhu2407 <giaphudiuhien@gmail.com>
2024-08-27 23:12:07 +0700

    27/8/24

---
+++ app/SignUp/page.tsx
@@ -0,0 +1,12 @@
+import SignUp from "@/components/Signup/SignUp";
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <SignUp />
+    </div>
+  );
+};
+
+export default page;
