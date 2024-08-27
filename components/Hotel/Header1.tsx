66ac375f5e3cbbc4259792ad72859a65f5b283b8
GiaPhu2407 <giaphudiuhien@gmail.com>
2024-08-27 23:25:43 +0700

    27/8/24

--- components/Hotel/Header1.tsx
+++ components/Hotel/Header1.tsx
@@ -37,12 +37,12 @@ const Header1 = () => {
               />
             </a>
             <div className="flex items-center md:order-2 space-x-1 md:space-x-2 rtl:space-x-reverse">
-              <a
-                href="/hotel"
+              <Link
+                href={""}
                 className="text-black  dark:text-white hover:bg-blue-500 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 py-2 md:px-5 md:py-2.5 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800"
               >
                 Login
-              </a>
+              </Link>
               <Link
                 href={"/SignUp"}
                 className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-10 py-10 md:px-5 md:py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
