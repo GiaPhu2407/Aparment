60b2f31a7af0602ac1cc3edbbc8c411a5ee27021
GiaPhu2407 <giaphudiuhien@gmail.com>
2024-09-01 20:51:53 +0700

    1/9/24

---
+++ app/ShowRestaurant/page.tsx
@@ -0,0 +1,12 @@
+import Restaurant from "@/components/Restaurant/Restaurant";
+import React from "react";
+Restaurant;
+const page = () => {
+  return (
+    <div>
+      <Restaurant />
+    </div>
+  );
+};
+
+export default page;
