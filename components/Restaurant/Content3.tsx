a26abf7b3525b7f405f38ecb4f09d43ef2e4ec9f
GiaPhu2407 <giaphudiuhien@gmail.com>
2024-09-01 21:00:37 +0700

    1/9/24

--- components/Restaurant/Content3.tsx
+++ components/Restaurant/Content3.tsx
@@ -1,7 +1,11 @@
 import React from "react";
-
+import ContentFood from "./ContentFood";
 const Content3 = () => {
-  return <div>Content</div>;
+  return (
+    <div>
+      <ContentFood />
+    </div>
+  );
 };
 
 export default Content3;
---
+++ components/Restaurant/ContentFood.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const ContentFood = () => {
+  return (
+    <div className="flex justify-center items-center mx-auto w-[70%]">
+      <div>
+        <p>
+          Một trải nghiệm trọn vẹn, đãi ngộ mọi giác quan, một khám phá ẩm thực
+          kỳ thú và duy nhất ven Thái Bình Dương.
+        </p>
+      </div>
+      <div>
+        <img
+          src="https://www.mo-residencesdanang.com/image/restaurant2.jpg"
+          alt=""
+        />
+      </div>
+    </div>
+  );
+};
+
+export default ContentFood;
