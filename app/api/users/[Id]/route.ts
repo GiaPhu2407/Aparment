138cf82fc8d958be088f2ce9018b1642d38a0ca2
GiaPhu2407 <giaphudiuhien@gmail.com>
2024-09-02 22:23:26 +0700

    2/9/24

--- app/api/users/[Id]/route.ts
+++ app/api/users/[Id]/route.ts
@@ -1,126 +1,126 @@
-import { NextRequest, NextResponse } from "next/server";
-import prisma from "@/prisma/client";
-import UserSchema from "@/app/llb/zod_schema/route";
-import { USER_NOT_EXIST } from "@/app/llb/constants";
+// import { NextRequest, NextResponse } from "next/server";
+// import prisma from "@/prisma/client";
+// import UserSchema from "@/app/llb/zod_schema/route";
+// import { USER_NOT_EXIST } from "@/app/llb/constants";
 
-// 1. getUserById - dựa trên id trả về thông tin của user
+// // 1. getUserById - dựa trên id trả về thông tin của user
 
-//2 deleteUserById - xoá user dựa trên id
+// //2 deleteUserById - xoá user dựa trên id
+
+// // export async function DELETE(
+// //   request: NextRequest,
+// //   { params }: { params: { Id: string } }
+// // ) {
+// //   const userId = parseInt(params.Id);
+// //   const ue = await prisma.user.findUnique({ where: { email: "", name: "" } });
+// //   if (ue === null) {
+// //     return NextResponse.json(
+// //       { message: `Khong co du lieu de xoa` },
+// //       { status: 200 }
+// //     );
+// //   } else {
+// //     const user = await prisma.user.delete({ where: { id: userId } });
+// //     return NextResponse.json(
+// //       { user, message: `API deletePostById: ${params.Id}` },
+// //       { status: 200, statusText: "The post is deleted successfully" }
+// //     );
+// //   }
+// // }
+
+// export async function GET(
+//   request: NextRequest,
+//   { params }: { params: { Id: string } }
+// ) {
+//   const userId = parseInt(params.Id);
+//   const user = await prisma.user.findUnique({
+//     where: { id: userId },
+//   });
+
+//   return NextResponse.json(
+//     { user, message: `Hien thi Id : ${params.Id} thành công` },
+//     { status: 200, statusText: "Gia Phu Perfect" }
+//   );
+// }
 
 // export async function DELETE(
 //   request: NextRequest,
 //   { params }: { params: { Id: string } }
 // ) {
-//   const userId = parseInt(params.Id);
-//   const ue = await prisma.user.findUnique({ where: { email: "", name: "" } });
-//   if (ue === null) {
+//   const userId = Number(params.Id);
+//   try {
+//     const deleteUser = await prisma.user.delete({
+//       where: { id: userId },
+//     });
+//     return NextResponse.json({ deleteUser }, { status: 200 });
+//   } catch (error: any) {
 //     return NextResponse.json(
-//       { message: `Khong co du lieu de xoa` },
-//       { status: 200 }
-//     );
-//   } else {
-//     const user = await prisma.user.delete({ where: { id: userId } });
-//     return NextResponse.json(
-//       { user, message: `API deletePostById: ${params.Id}` },
-//       { status: 200, statusText: "The post is deleted successfully" }
+//       { error_code: USER_NOT_EXIST.error_code, cause: USER_NOT_EXIST.message },
+//       { status: 400 }
 //     );
 //   }
 // }
 
-export async function GET(
-  request: NextRequest,
-  { params }: { params: { Id: string } }
-) {
-  const userId = parseInt(params.Id);
-  const user = await prisma.user.findUnique({
-    where: { id: userId },
-  });
+// //3 updateUserById - cập nhật dự liệu cho user id( dữ liệu mới trong request body)
+// // export async function PUT(
+// //   request: NextRequest,
+// //   { params }: { params: { Id: string } }
+// // ) {
+// //   const userId = parseInt(params.Id);
+// //   const user = await prisma.user.update({
+// //     where: { id: userId },
+// //     data: { name: "Viet Thanh", email: "caongogiaphu@gamil.com" },
+// //   });
+// //   return NextResponse.json(
+// //     { user, message: `API deletePostById: ${params.Id}` },
+// //     { status: 200, statusText: "The post is deleted successfully" }
+// //   );
+// // }
 
-  return NextResponse.json(
-    { user, message: `Hien thi Id : ${params.Id} thành công` },
-    { status: 200, statusText: "Gia Phu Perfect" }
-  );
-}
-
-export async function DELETE(
-  request: NextRequest,
-  { params }: { params: { Id: string } }
-) {
-  const userId = Number(params.Id);
-  try {
-    const deleteUser = await prisma.user.delete({
-      where: { id: userId },
-    });
-    return NextResponse.json({ deleteUser }, { status: 200 });
-  } catch (error: any) {
-    return NextResponse.json(
-      { error_code: USER_NOT_EXIST.error_code, cause: USER_NOT_EXIST.message },
-      { status: 400 }
-    );
-  }
-}
-
-//3 updateUserById - cập nhật dự liệu cho user id( dữ liệu mới trong request body)
 // export async function PUT(
 //   request: NextRequest,
 //   { params }: { params: { Id: string } }
 // ) {
 //   const userId = parseInt(params.Id);
-//   const user = await prisma.user.update({
-//     where: { id: userId },
-//     data: { name: "Viet Thanh", email: "caongogiaphu@gamil.com" },
+//   const body = await request.json();
+//   // try {
+//   // Read and parse data from request body
+
+//   // Validate the incoming data against UserSchema
+//   const userValidateCheck = UserSchema.safeParse({
+//     email: body.email,
+//     name: body.name,
 //   });
-//   return NextResponse.json(
-//     { user, message: `API deletePostById: ${params.Id}` },
-//     { status: 200, statusText: "The post is deleted successfully" }
-//   );
-// }
 
-export async function PUT(
-  request: NextRequest,
-  { params }: { params: { Id: string } }
-) {
-  const userId = parseInt(params.Id);
-  const body = await request.json();
-  // try {
-  // Read and parse data from request body
-
-  // Validate the incoming data against UserSchema
-  const userValidateCheck = UserSchema.safeParse({
-    email: body.email,
-    name: body.name,
-  });
-
-  if (!userValidateCheck.success) {
+//   if (!userValidateCheck.success) {
     
-    return NextResponse.json(userValidateCheck.error.errors, { status: 400 });
-  }
+//     return NextResponse.json(userValidateCheck.error.errors, { status: 400 });
+//   }
 
-  // Check if the email already exists for another user
-  const ue = await prisma.user.findUnique({
-    where: { email: body.email },
-  });
+//   // Check if the email already exists for another user
+//   const ue = await prisma.user.findUnique({
+//     where: { email: body.email },
+//   });
 
-  // If ue exists and is not the same user
-  if (ue !== null) {
-    // Update user data
-    return NextResponse.json({ message: "Email đã tồn tại" }, { status: 400 });
-  } else {
-    const user = await prisma.user.update({
-      where: { id: userId },
-      data: { name: body.name, email: body.email },
-    });
+//   // If ue exists and is not the same user
+//   if (ue !== null) {
+//     // Update user data
+//     return NextResponse.json({ message: "Email đã tồn tại" }, { status: 400 });
+//   } else {
+//     const user = await prisma.user.update({
+//       where: { id: userId },
+//       data: { name: body.name, email: body.email },
+//     });
 
-    return NextResponse.json(
-      { user, message: `Cập nhật dữ liệu từ id ${params.Id} thành công` },
-      { status: 200 }
-    );
-  }
-  // } catch (error) {
-  //   console.error("Error:", error);
-  //   return NextResponse.json(
-  //     { error: "Internal server error" },
-  //     { status: 500 }
-  //   );
-  // }
-}
+//     return NextResponse.json(
+//       { user, message: `Cập nhật dữ liệu từ id ${params.Id} thành công` },
+//       { status: 200 }
+//     );
+//   }
+//   // } catch (error) {
+//   //   console.error("Error:", error);
+//   //   return NextResponse.json(
+//   //     { error: "Internal server error" },
+//   //     { status: 500 }
+//   //   );
+//   // }
+// }
--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,37 +1,97 @@
-import UserSchema from "@/app/llb/zod_schema/route";
-import prisma from "@/prisma/client";
-import { request } from "http";
-import { NextRequest, NextResponse } from "next/server";
-import { z } from "zod";
+// import UserSchema from "@/app/llb/zod_schema/route";
+// import prisma from "@/prisma/client";
+// import { request } from "http";
+// import { NextRequest, NextResponse } from "next/server";
+// import { z } from "zod";
 
-// 1. Lấy tất cả các users
-export async function GET() {
-  const user = await prisma.user.findMany();
+// // 1. Lấy tất cả các users
+// export async function GET() {
+//   const user = await prisma.user.findMany();
 
-  return NextResponse.json(
-    { user, message: `OK` },
-    { status: 200, statusText: "Gia Phu oKE" }
-  );
-}
+//   return NextResponse.json(
+//     { user, message: `OK` },
+//     { status: 200, statusText: "Gia Phu oKE" }
+//   );
+// }
 
-// export async function POST(request: NextRequest) {
+// // export async function POST(request: NextRequest) {
+// //   const body = await request.json();
+
+// //   const UserSchema = z.object({
+// //     email: z.string().email({ message: "Email khong hop le" }),
+// //     name: z
+// //       .string()
+// //       .min(8, { message: "Tên cần dài hơn hoặc bằng 8 kí tự" })
+// //       .max(255, { message: "Tên có độ dài tối đa là 255 kí tự" }),
+// //   });
+
+// //   const userValidateCheck = UserSchema.safeParse({
+// //     email: body.email,
+// //     name: body.name,
+// //   });
+
+// //   if (!userValidateCheck.success) {
+// //     return NextResponse.json(userValidateCheck.error.errors, { status: 200 });
+// //   } else {
+// //     const ue = await prisma.user.findUnique({
+// //       where: { email: body.email },
+// //     });
+// //     if (ue == null) {
+// //       const user = await prisma.user.create({
+// //         data: {
+// //           email: body.email,
+// //           name: body.name,
+// //         },
+// //       });
+
+// //       return NextResponse.json(
+// //         { user, message: `Thêm mới thành công` },
+// //         { status: 201 }
+// //       );
+// //     } else {
+// //       return NextResponse.json(
+// //         { message: "Email đã tồn tại" },
+// //         { status: 200 }
+// //       );
+// //     }
+// //     //   // c. Tiến hành thêm mới dữ liệu vào DB sử dụng prisma client
+// //     //   const user = await prisma.user.create({
+// //     //     data: {
+// //     //       email: body.email,
+// //     //       name: body.name,
+// //     //     },
+// //     //   });
+// //     //   //d. Trả về kết quả là dữ liệu vừa được tạo cùng với status:201
+// //     //   return NextResponse.json(
+// //     //     { user, message: `Thêm mới thành công` },
+// //     //     { status: 201 }
+// //     //   );
+// //     // }
+// //   }
+// // }
+
+// //3.Xoá tất cả các users
+
+// export async function POST(
+//   request: NextRequest,
+//   { params }: { params: { Id: string } }
+// ) {
 //   const body = await request.json();
 
 //   const UserSchema = z.object({
 //     email: z.string().email({ message: "Email khong hop le" }),
 //     name: z
 //       .string()
-//       .min(8, { message: "Tên cần dài hơn hoặc bằng 8 kí tự" })
-//       .max(255, { message: "Tên có độ dài tối đa là 255 kí tự" }),
+//       .min(8, { message: "Ten ky tu phai dai hon 8 ky tu" })
+//       .max(255, { message: "Ten khong duoc dai qua 255 ky tu" }),
 //   });
 
-//   const userValidateCheck = UserSchema.safeParse({
+//   const Check = UserSchema.safeParse({
 //     email: body.email,
 //     name: body.name,
 //   });
-
-//   if (!userValidateCheck.success) {
-//     return NextResponse.json(userValidateCheck.error.errors, { status: 200 });
+//   if (!Check.success) {
+//     return NextResponse.json(Check.error.errors, { status: 200 });
 //   } else {
 //     const ue = await prisma.user.findUnique({
 //       where: { email: body.email },
@@ -43,81 +103,21 @@ export async function GET() {
 //           name: body.name,
 //         },
 //       });
-
 //       return NextResponse.json(
 //         { user, message: `Thêm mới thành công` },
 //         { status: 201 }
 //       );
 //     } else {
-//       return NextResponse.json(
-//         { message: "Email đã tồn tại" },
-//         { status: 200 }
-//       );
+//       return NextResponse.json({ message: "Email đã tồn tại" });
 //     }
-//     //   // c. Tiến hành thêm mới dữ liệu vào DB sử dụng prisma client
-//     //   const user = await prisma.user.create({
-//     //     data: {
-//     //       email: body.email,
-//     //       name: body.name,
-//     //     },
-//     //   });
-//     //   //d. Trả về kết quả là dữ liệu vừa được tạo cùng với status:201
-//     //   return NextResponse.json(
-//     //     { user, message: `Thêm mới thành công` },
-//     //     { status: 201 }
-//     //   );
-//     // }
 //   }
 // }
+// export async function DELETE(request: NextRequest) {
+//   const user = await prisma.user.deleteMany();
+//   return NextResponse.json(
+//     { user, message: `Xoá tất cả user` },
+//     { status: 200 }
+//   );
+// }
 
-//3.Xoá tất cả các users
-
-export async function POST(
-  request: NextRequest,
-  { params }: { params: { Id: string } }
-) {
-  const body = await request.json();
-
-  const UserSchema = z.object({
-    email: z.string().email({ message: "Email khong hop le" }),
-    name: z
-      .string()
-      .min(8, { message: "Ten ky tu phai dai hon 8 ky tu" })
-      .max(255, { message: "Ten khong duoc dai qua 255 ky tu" }),
-  });
-
-  const Check = UserSchema.safeParse({
-    email: body.email,
-    name: body.name,
-  });
-  if (!Check.success) {
-    return NextResponse.json(Check.error.errors, { status: 200 });
-  } else {
-    const ue = await prisma.user.findUnique({
-      where: { email: body.email },
-    });
-    if (ue == null) {
-      const user = await prisma.user.create({
-        data: {
-          email: body.email,
-          name: body.name,
-        },
-      });
-      return NextResponse.json(
-        { user, message: `Thêm mới thành công` },
-        { status: 201 }
-      );
-    } else {
-      return NextResponse.json({ message: "Email đã tồn tại" });
-    }
-  }
-}
-export async function DELETE(request: NextRequest) {
-  const user = await prisma.user.deleteMany();
-  return NextResponse.json(
-    { user, message: `Xoá tất cả user` },
-    { status: 200 }
-  );
-}
-
-//zod để kiểm tra tính hợp lệ của dữ liệu
+// //zod để kiểm tra tính hợp lệ của dữ liệu
