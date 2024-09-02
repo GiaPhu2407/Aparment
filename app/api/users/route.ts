2f63af15624a3184cf7a13aa0105998f4f8b0622
GiaPhu2407 <giaphudiuhien@gmail.com>
2024-09-02 21:59:19 +0700

    2/9/24

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,3 +1,4 @@
+import UserSchema from "@/app/llb/zod-schema/route";
 import prisma from "@/prisma/client";
 import { request } from "http";
 import { NextRequest, NextResponse } from "next/server";
@@ -13,24 +14,84 @@ export async function GET() {
   );
 }
 
-export async function POST(request: NextRequest) {
+// export async function POST(request: NextRequest) {
+//   const body = await request.json();
+
+//   const UserSchema = z.object({
+//     email: z.string().email({ message: "Email khong hop le" }),
+//     name: z
+//       .string()
+//       .min(8, { message: "Tên cần dài hơn hoặc bằng 8 kí tự" })
+//       .max(255, { message: "Tên có độ dài tối đa là 255 kí tự" }),
+//   });
+
+//   const userValidateCheck = UserSchema.safeParse({
+//     email: body.email,
+//     name: body.name,
+//   });
+
+//   if (!userValidateCheck.success) {
+//     return NextResponse.json(userValidateCheck.error.errors, { status: 200 });
+//   } else {
+//     const ue = await prisma.user.findUnique({
+//       where: { email: body.email },
+//     });
+//     if (ue == null) {
+//       const user = await prisma.user.create({
+//         data: {
+//           email: body.email,
+//           name: body.name,
+//         },
+//       });
+
+//       return NextResponse.json(
+//         { user, message: `Thêm mới thành công` },
+//         { status: 201 }
+//       );
+//     } else {
+//       return NextResponse.json(
+//         { message: "Email đã tồn tại" },
+//         { status: 200 }
+//       );
+//     }
+//     //   // c. Tiến hành thêm mới dữ liệu vào DB sử dụng prisma client
+//     //   const user = await prisma.user.create({
+//     //     data: {
+//     //       email: body.email,
+//     //       name: body.name,
+//     //     },
+//     //   });
+//     //   //d. Trả về kết quả là dữ liệu vừa được tạo cùng với status:201
+//     //   return NextResponse.json(
+//     //     { user, message: `Thêm mới thành công` },
+//     //     { status: 201 }
+//     //   );
+//     // }
+//   }
+// }
+
+//3.Xoá tất cả các users
+
+export async function POST(
+  request: NextRequest,
+  { params }: { params: { Id: string } }
+) {
   const body = await request.json();
 
-  const UserSchema = z.object({
+  const Userschema = z.object({
     email: z.string().email({ message: "Email khong hop le" }),
     name: z
       .string()
-      .min(8, { message: "Tên cần dài hơn hoặc bằng 8 kí tự" })
-      .max(255, { message: "Tên có độ dài tối đa là 255 kí tự" }),
+      .min(8, { message: "Ten ky tu phai dai hon 8 ky tu" })
+      .max(255, { message: "Ten khong duoc dai qua 255 ky tu" }),
   });
 
-  const userValidateCheck = UserSchema.safeParse({
+  const Check = UserSchema.safeParse({
     email: body.email,
     name: body.name,
   });
-
-  if (!userValidateCheck.success) {
-    return NextResponse.json(userValidateCheck.error.errors, { status: 200 });
+  if (!Check.success) {
+    return NextResponse.json(Check.error.errors, { status: 200 });
   } else {
     const ue = await prisma.user.findUnique({
       where: { email: body.email },
@@ -42,36 +103,15 @@ export async function POST(request: NextRequest) {
           name: body.name,
         },
       });
-
       return NextResponse.json(
         { user, message: `Thêm mới thành công` },
         { status: 201 }
       );
     } else {
-      return NextResponse.json(
-        { message: "Email đã tồn tại" },
-        { status: 200 }
-      );
+      return NextResponse.json({ message: "Email đã tồn tại" });
     }
-    //   // c. Tiến hành thêm mới dữ liệu vào DB sử dụng prisma client
-    //   const user = await prisma.user.create({
-    //     data: {
-    //       email: body.email,
-    //       name: body.name,
-    //     },
-    //   });
-    //   //d. Trả về kết quả là dữ liệu vừa được tạo cùng với status:201
-    //   return NextResponse.json(
-    //     { user, message: `Thêm mới thành công` },
-    //     { status: 201 }
-    //   );
-    // }
   }
 }
-
-
-
-//3.Xoá tất cả các users
 export async function DELETE(request: NextRequest) {
   const user = await prisma.user.deleteMany();
   return NextResponse.json(
