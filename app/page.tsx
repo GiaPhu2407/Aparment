40bdad9cc89d52f90cd9dadc27960de723be32e1
GiaPhu2407 <giaphudiuhien@gmail.com>
2024-09-04 09:24:38 +0700

    4/9/24

--- app/page.tsx
+++ app/page.tsx
@@ -1,52 +1,52 @@
-"use client";
-import { useState } from "react";
+// "use client";
+// import { useState } from "react";
 
-export default function Home() {
-  const [email, setEmail] = useState("");
-  const [name, setName] = useState("");
-  const [message, setMessage] = useState("");
+// export default function Home() {
+//   const [email, setEmail] = useState("");
+//   const [name, setName] = useState("");
+//   const [message, setMessage] = useState("");
 
-  const handleSubmit = async (e: any) => {
-    e.preventDefault();
+//   const handleSubmit = async (e: any) => {
+//     e.preventDefault();
 
-    const response = await fetch("/api/users", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ email, name }),
-    });
+//     const response = await fetch("/api/users", {
+//       method: "POST",
+//       headers: { "Content-Type": "application/json" },
+//       body: JSON.stringify({ email, name }),
+//     });
 
-    const result = await response.json();
-    setMessage(result.message || result.error || "Có lỗi xảy ra");
-  };
-  return (
-    <div>
-      <h1>Thêm người dùng mới</h1>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>Email:</label>
-          <input
-            type="email"
-            name="email"
-            className="text-black"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-        </div>
-        <div>
-          <label>Tên:</label>
-          <input
-            type="text"
-            name="name"
-            className="text-black"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            required
-          />
-        </div>
-        <button type="submit">Thêm mới</button>
-      </form>
-      {message && <p>{message}</p>}
-    </div>
-  );
-}
+//     const result = await response.json();
+//     setMessage(result.message || result.error || "Có lỗi xảy ra");
+//   };
+//   return (
+//     <div>
+//       <h1>Thêm người dùng mới</h1>
+//       <form onSubmit={handleSubmit}>
+//         <div>
+//           <label>Email:</label>
+//           <input
+//             type="email"
+//             name="email"
+//             className="text-black"
+//             value={email}
+//             onChange={(e) => setEmail(e.target.value)}
+//             required
+//           />
+//         </div>
+//         <div>
+//           <label>Tên:</label>
+//           <input
+//             type="text"
+//             name="name"
+//             className="text-black"
+//             value={name}
+//             onChange={(e) => setName(e.target.value)}
+//             required
+//           />
+//         </div>
+//         <button type="submit">Thêm mới</button>
+//       </form>
+//       {message && <p>{message}</p>}
+//     </div>
+//   );
+// }
