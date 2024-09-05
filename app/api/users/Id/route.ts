55b4565704f4359b2042e87215615642d119e227
GiaPhu2407 <giaphudiuhien@gmail.com>
2024-09-05 20:37:43 +0700

    5/9/24

--- app/api/users/Id/route.ts
+++ app/api/users/Id/route.ts
@@ -88,33 +88,33 @@ export async function DELETE(
 //     );
 //   }
 // }
+
 export async function PUT(
   request: NextRequest,
   { params }: { params: { Id: string } }
 ) {
-  const body = await request.json();
   const userId = parseInt(params.Id);
+  const body = await request.json();
 
   const Check = Schema.safeParse({
     email: body.email,
     name: body.name,
   });
-
   if (!Check.success) {
     return NextResponse.json({ error: Check.error }, { status: 200 });
   }
   const ue = await prisma.user.findUnique({
-    where: { id: userId },
+    where: { email: body.email },
   });
   if (ue != null) {
-    return NextResponse.json({ error: "Email da ton tai" }, { status: 400 });
+    return NextResponse.json({ error: "Email đã tồn tại" }, { status: 200 });
   } else {
     const user = await prisma.user.update({
       where: { id: userId },
       data: { email: body.email, name: body.name },
     });
     return NextResponse.json(
-      { message: `Cap nhat Id ${params.Id} thanh cong` },
+      { message: `Cap nhat Id ${params.Id} thành công` },
       { status: 200 }
     );
   }
