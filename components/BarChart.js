fa043a547e0394f6177bf3a3667287056c0f4562
GiaPhu2407 <giaphudiuhien@gmail.com>
2024-09-08 20:27:18 +0700

    8/9/24

--- components/BarChart.js
+++ components/BarChart.js
@@ -1,8 +1,24 @@
 "use client";
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Chart } from "chart.js/auto";
+import axios from "axios";
 const BarChart = () => {
   const chartRef = useRef(null);
+  const [chartData, setCharData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios("http://dummyjson.com/users");
+      if (response.status != 200) {
+        console.error("Bad response");
+      }
+      const data = await response.data;
+      // console.log(data);
+      const firstSix = data.users.splice(0, 6);
+      setCharData(firstSix);
+    };
+    fetchData();
+  }, []);
 
   useEffect(() => {
     if (chartRef.current) {
@@ -12,22 +28,18 @@ const BarChart = () => {
 
       const context = chartRef.current.getContext("2d");
 
+      const label = chartData.map((items) => items.firstName);
+      const data = chartData.map((items) => items.weight);
+
       const newChart = new Chart(context, {
         type: "bar",
         data: {
-          labels: [
-            "January",
-            "February",
-            "March",
-            "April",
-            "May",
-            "June",
-            "July",
-          ],
+          labels: label,
           datasets: [
             {
+              barThickness: 50,
               label: "# of Votes",
-              data: [12, 19, 3, 5, 2, 3],
+              data: data,
               backgroundColor: [
                 "rgba(255, 99, 132, 0.2)",
                 "rgba(54, 162, 235, 0.2)",
@@ -42,10 +54,20 @@ const BarChart = () => {
                 "rgba(255, 206, 86, 1)",
                 "rgba(75, 192, 192, 1)",
               ],
+              borderWidth: 1,
+              borderRadius: 10,
             },
           ],
         },
         options: {
+          plugins: {
+            title: {
+              display: true,
+              text: "User Weight Distribution",
+              fontSize: 20,
+              padding: 20,
+            },
+          },
           responsive: true,
           scales: {
             x: {
@@ -60,7 +82,7 @@ const BarChart = () => {
 
       chartRef.current.chart = newChart;
     }
-  }, []);
+  }, [chartData]);
   return (
     <div style={{ position: "relative", width: "50vw", height: "80vh" }}>
       <canvas id="myChart" ref={chartRef}></canvas>
