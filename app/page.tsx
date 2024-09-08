e05c45c328767b3f9f90715ccb26765a57b4e535
GiaPhu2407 <giaphudiuhien@gmail.com>
2024-09-08 10:58:35 +0700

    8/9/24

--- app/page.tsx
+++ app/page.tsx
@@ -1,40 +1,14 @@
-/*
- * Install the Generative AI SDK
- *
- * $ npm install @google/generative-ai
- */
+import BarChart from "@/components/BarChart";
 
-const {
-  GoogleGenerativeAI,
-  HarmCategory,
-  HarmBlockThreshold,
-} = require("@google/generative-ai");
-
-const apiKey = process.env.GEMINI_API_KEY;
-const genAI = new GoogleGenerativeAI(apiKey);
-
-const model = genAI.getGenerativeModel({
-  model: "gemini-1.0-pro",
-});
-
-const generationConfig = {
-  temperature: 0.9,
-  topP: 1,
-  maxOutputTokens: 2048,
-  responseMimeType: "text/plain",
+const Home = () => {
+  return (
+    <div className="container">
+      Chart js
+      <div>
+        <BarChart />
+      </div>
+    </div>
+  );
 };
 
-async function run() {
-  const chatSession = model.startChat({
-    generationConfig,
- // safetySettings: Adjust safety settings
- // See https://ai.google.dev/gemini-api/docs/safety-settings
-    history: [
-    ],
-  });
-
-  const result = await chatSession.sendMessage("INSERT_INPUT_HERE");
-  console.log(result.response.text());
-}
-
-run();
+export default Home;
---
+++ components/BarChart.js
@@ -0,0 +1,63 @@
+"use client";
+import React, { useEffect, useRef } from "react";
+import { Chart } from "chart.js/auto";
+const BarChart = () => {
+  const chartRef = useRef(null);
+
+  useEffect(() => {
+    if (chartRef.current) {
+      if (chartRef.current.chart) {
+        chartRef.current.chart.destroy();
+      }
+
+      const context = chartRef.current.getContext("2d");
+
+      const newChart = new Chart(context, {
+        type: "bar",
+        data: {
+          labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+          datasets: [
+            {
+              label: "# of Votes",
+              data: [12, 19, 3, 5, 2, 3],
+              backgroundColor: [
+                "rgba(255, 99, 132, 0.2)",
+                "rgba(54, 162, 235, 0.2)",
+                "rgba(255, 206, 86, 0.2)",
+                "rgba(75, 192, 192, 0.2)",
+                "rgba(153, 102, 255, 0.2)",
+                "rgba(255, 159, 64, 0.2)",
+              ],
+              borderColor: [
+                "rgba(255, 99, 132, 1)",
+                "rgba(54, 162, 235, 1)",
+                "rgba(255, 206, 86, 1)",
+                "rgba(75, 192, 192, 1)",
+              ],
+            },
+          ],
+        },
+        options: {
+          responsive: true,
+          scales: {
+            x: {
+              type: "category",
+            },
+            y: {
+              beginAtZero: true,
+            },
+          },
+        },
+      });
+
+      chartRef.current.chart = newChart;
+    }
+  }, []);
+  return (
+    <div style={{ position: "relative", width: "50vw", height: "80vh" }}>
+      <canvas id="myChart" ref={chartRef}></canvas>
+    </div>
+  );
+};
+
+export default BarChart;
