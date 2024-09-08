e14d76bc1f97a681b749c73eeef64a657e34ca24
GiaPhu2407 <giaphudiuhien@gmail.com>
2024-09-08 22:09:19 +0700

    8/9/24

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
 import BarChart from "@/components/BarChart";
-
+import page from "./ShowDashBoard/page";
+import SidebarDemo from "./component/dashboard";
 const Home = () => {
   return (
-    <div className="container">
-      Chart js
-      <div>
-        <BarChart />
-      </div>
+    <div>
+      <SidebarDemo />
     </div>
   );
 };
