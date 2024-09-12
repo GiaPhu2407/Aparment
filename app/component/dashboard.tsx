f9bf4f09ed751f5fc75e4f6f16d4d68da835862e
GiaPhu2407 <giaphudiuhien@gmail.com>
2024-09-12 14:14:30 +0700

    12/9/24

--- app/component/dashboard.tsx
+++ app/component/dashboard.tsx
@@ -295,15 +295,17 @@ const Dashboard = () => {
       </div>
       {/* Content 2 */}
       <div className="relative">
-        <div className="absolute top-[350px] right-36 w-[100px] h-[50px] mt-2 ">
+        <div className="absolute top-[350px] right-44 w-[100px] h-[50px] mt-2 ">
           <p className="font-extrabold">Activities</p>
           <div className="flex my-5">
-            <div className="mt-1  ">
-              <img
-                src="https://scontent.fsgn5-12.fna.fbcdn.net/v/t39.30808-6/428603879_3699291333727981_8450238390845543097_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=6ee11a&_nc_eui2=AeF8eAl4DqSiHc45WD_3ZUa0Yj2_k1pIrCxiPb-TWkisLKdt4xrCZnSawKeji-j5xGqR90NFt9JrVnVNHzicN-9h&_nc_ohc=cBtDM4VOrzgQ7kNvgGzZVqa&_nc_ht=scontent.fsgn5-12.fna&_nc_gid=A828t2YNiXD0IX9947EaHtO&oh=00_AYCPO6al1z14lOE9jh_4FC8I62G66_fmYVo3qVbfU0VEpw&oe=66E85985"
-                alt=""
-                className="text-sm rounded-xl"
-              />
+            <div className="avatar   ">
+              <div className="w-8 h-8">
+                <img
+                  src="https://scontent.fsgn5-12.fna.fbcdn.net/v/t39.30808-6/428603879_3699291333727981_8450238390845543097_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=6ee11a&_nc_eui2=AeF8eAl4DqSiHc45WD_3ZUa0Yj2_k1pIrCxiPb-TWkisLKdt4xrCZnSawKeji-j5xGqR90NFt9JrVnVNHzicN-9h&_nc_ohc=cBtDM4VOrzgQ7kNvgGzZVqa&_nc_ht=scontent.fsgn5-12.fna&_nc_gid=A828t2YNiXD0IX9947EaHtO&oh=00_AYCPO6al1z14lOE9jh_4FC8I62G66_fmYVo3qVbfU0VEpw&oe=66E85985"
+                  alt=""
+                  className=" rounded-full"
+                />
+              </div>
             </div>
             <div>
               <p className="whitespace-nowrap ml-2">
@@ -313,12 +315,14 @@ const Dashboard = () => {
             </div>
           </div>
           <div className="flex my-5">
-            <div className="mt-1 ">
-              <img
-                src="https://scontent.fsgn5-12.fna.fbcdn.net/v/t39.30808-6/428603879_3699291333727981_8450238390845543097_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=6ee11a&_nc_eui2=AeF8eAl4DqSiHc45WD_3ZUa0Yj2_k1pIrCxiPb-TWkisLKdt4xrCZnSawKeji-j5xGqR90NFt9JrVnVNHzicN-9h&_nc_ohc=cBtDM4VOrzgQ7kNvgGzZVqa&_nc_ht=scontent.fsgn5-12.fna&_nc_gid=A828t2YNiXD0IX9947EaHtO&oh=00_AYCPO6al1z14lOE9jh_4FC8I62G66_fmYVo3qVbfU0VEpw&oe=66E85985"
-                alt=""
-                className="text-sm rounded-xl"
-              />
+            <div className="avatar   ">
+              <div className="w-8 h-8">
+                <img
+                  src="https://scontent.fsgn5-12.fna.fbcdn.net/v/t39.30808-6/428603879_3699291333727981_8450238390845543097_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=6ee11a&_nc_eui2=AeF8eAl4DqSiHc45WD_3ZUa0Yj2_k1pIrCxiPb-TWkisLKdt4xrCZnSawKeji-j5xGqR90NFt9JrVnVNHzicN-9h&_nc_ohc=cBtDM4VOrzgQ7kNvgGzZVqa&_nc_ht=scontent.fsgn5-12.fna&_nc_gid=A828t2YNiXD0IX9947EaHtO&oh=00_AYCPO6al1z14lOE9jh_4FC8I62G66_fmYVo3qVbfU0VEpw&oe=66E85985"
+                  alt=""
+                  className=" rounded-full"
+                />
+              </div>
             </div>
             <div>
               <p className="whitespace-nowrap ml-2">
@@ -328,25 +332,31 @@ const Dashboard = () => {
             </div>
           </div>
           <div className="flex my-5">
-            <div className="mt-1 ">
-              <img
-                src="https://scontent.fsgn5-12.fna.fbcdn.net/v/t39.30808-6/428603879_3699291333727981_8450238390845543097_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=6ee11a&_nc_eui2=AeF8eAl4DqSiHc45WD_3ZUa0Yj2_k1pIrCxiPb-TWkisLKdt4xrCZnSawKeji-j5xGqR90NFt9JrVnVNHzicN-9h&_nc_ohc=cBtDM4VOrzgQ7kNvgGzZVqa&_nc_ht=scontent.fsgn5-12.fna&_nc_gid=A828t2YNiXD0IX9947EaHtO&oh=00_AYCPO6al1z14lOE9jh_4FC8I62G66_fmYVo3qVbfU0VEpw&oe=66E85985"
-                alt=""
-                className="text-sm rounded-xl"
-              />
+            <div className="avatar   ">
+              <div className="w-8 h-8">
+                <img
+                  src="https://scontent.fsgn5-12.fna.fbcdn.net/v/t39.30808-6/428603879_3699291333727981_8450238390845543097_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=6ee11a&_nc_eui2=AeF8eAl4DqSiHc45WD_3ZUa0Yj2_k1pIrCxiPb-TWkisLKdt4xrCZnSawKeji-j5xGqR90NFt9JrVnVNHzicN-9h&_nc_ohc=cBtDM4VOrzgQ7kNvgGzZVqa&_nc_ht=scontent.fsgn5-12.fna&_nc_gid=A828t2YNiXD0IX9947EaHtO&oh=00_AYCPO6al1z14lOE9jh_4FC8I62G66_fmYVo3qVbfU0VEpw&oe=66E85985"
+                  alt=""
+                  className=" rounded-full"
+                />
+              </div>
             </div>
             <div>
               <p className="whitespace-nowrap ml-2">Submitted a bug</p>
-              <p className="text-sm text-gray-500 ml-2">Yesterday 12:39 AM</p>
+              <p className="text-sm text-gray-500 ml-2 whitespace-nowrap">
+                Yesterday 12:39 AM
+              </p>
             </div>
           </div>
           <div className="flex my-5">
-            <div className="mt-1 ">
-              <img
-                src="https://scontent.fsgn5-12.fna.fbcdn.net/v/t39.30808-6/428603879_3699291333727981_8450238390845543097_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=6ee11a&_nc_eui2=AeF8eAl4DqSiHc45WD_3ZUa0Yj2_k1pIrCxiPb-TWkisLKdt4xrCZnSawKeji-j5xGqR90NFt9JrVnVNHzicN-9h&_nc_ohc=cBtDM4VOrzgQ7kNvgGzZVqa&_nc_ht=scontent.fsgn5-12.fna&_nc_gid=A828t2YNiXD0IX9947EaHtO&oh=00_AYCPO6al1z14lOE9jh_4FC8I62G66_fmYVo3qVbfU0VEpw&oe=66E85985"
-                alt=""
-                className="text-sm rounded-xl"
-              />
+            <div className="avatar   ">
+              <div className="w-8 h-8">
+                <img
+                  src="https://scontent.fsgn5-12.fna.fbcdn.net/v/t39.30808-6/428603879_3699291333727981_8450238390845543097_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=6ee11a&_nc_eui2=AeF8eAl4DqSiHc45WD_3ZUa0Yj2_k1pIrCxiPb-TWkisLKdt4xrCZnSawKeji-j5xGqR90NFt9JrVnVNHzicN-9h&_nc_ohc=cBtDM4VOrzgQ7kNvgGzZVqa&_nc_ht=scontent.fsgn5-12.fna&_nc_gid=A828t2YNiXD0IX9947EaHtO&oh=00_AYCPO6al1z14lOE9jh_4FC8I62G66_fmYVo3qVbfU0VEpw&oe=66E85985"
+                  alt=""
+                  className=" rounded-full"
+                />
+              </div>
             </div>
             <div>
               <p className="whitespace-nowrap ml-2">
@@ -361,12 +371,14 @@ const Dashboard = () => {
             </div>
           </div>
           <div className="flex my-5">
-            <div className="mt-1 ">
-              <img
-                src="https://scontent.fsgn5-12.fna.fbcdn.net/v/t39.30808-6/428603879_3699291333727981_8450238390845543097_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=6ee11a&_nc_eui2=AeF8eAl4DqSiHc45WD_3ZUa0Yj2_k1pIrCxiPb-TWkisLKdt4xrCZnSawKeji-j5xGqR90NFt9JrVnVNHzicN-9h&_nc_ohc=cBtDM4VOrzgQ7kNvgGzZVqa&_nc_ht=scontent.fsgn5-12.fna&_nc_gid=A828t2YNiXD0IX9947EaHtO&oh=00_AYCPO6al1z14lOE9jh_4FC8I62G66_fmYVo3qVbfU0VEpw&oe=66E85985"
-                alt=""
-                className="text-sm rounded-xl"
-              />
+            <div className="avatar   ">
+              <div className="w-8 h-8">
+                <img
+                  src="https://scontent.fsgn5-12.fna.fbcdn.net/v/t39.30808-6/428603879_3699291333727981_8450238390845543097_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=6ee11a&_nc_eui2=AeF8eAl4DqSiHc45WD_3ZUa0Yj2_k1pIrCxiPb-TWkisLKdt4xrCZnSawKeji-j5xGqR90NFt9JrVnVNHzicN-9h&_nc_ohc=cBtDM4VOrzgQ7kNvgGzZVqa&_nc_ht=scontent.fsgn5-12.fna&_nc_gid=A828t2YNiXD0IX9947EaHtO&oh=00_AYCPO6al1z14lOE9jh_4FC8I62G66_fmYVo3qVbfU0VEpw&oe=66E85985"
+                  alt=""
+                  className=" rounded-full"
+                />
+              </div>
             </div>
             <div>
               <p className="whitespace-nowrap ml-2">
